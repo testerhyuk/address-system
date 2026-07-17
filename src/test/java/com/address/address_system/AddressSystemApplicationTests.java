c72b6a84122fa5da74b3f152b0d95ec44acce7a6
Hyuk <testerhyuk214@gmail.com>
2026-07-17 18:20:25 +0900

    Spring Boot 애플리케이션 기반 설정 구성

--- src/test/java/com/address/address_system/AddressSystemApplicationTests.java
+++ src/test/java/com/address/address_system/AddressSystemApplicationTests.java
@@ -1,13 +1,25 @@
 package com.address.address_system;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.Clock;
+import java.time.ZoneOffset;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AddressSystemApplicationTests {
+
+	@Autowired
+	private Clock clock;
 
 	@Test
 	void contextLoads() {
+		assertThat(clock.getZone()).isEqualTo(ZoneOffset.UTC);
 	}
 
 }
