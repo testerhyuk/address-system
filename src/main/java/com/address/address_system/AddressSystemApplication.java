7ec5534f0740b90982fb2126ce744ba15aab08a7
Hyuk <testerhyuk214@gmail.com>
2026-07-17 17:58:30 +0900

    Spring Boot 프로젝트 초기 구조 구성

---
+++ src/main/java/com/address/address_system/AddressSystemApplication.java
@@ -0,0 +1,13 @@
+package com.address.address_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/address/address_system/AddressSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.address.address_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
